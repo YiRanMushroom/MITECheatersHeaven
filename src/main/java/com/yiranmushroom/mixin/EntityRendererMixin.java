35c19964d4526474b45734e282995d1f4bba36c4
Yiran Wang <114495322+YiRanMushroom@users.noreply.github.com>
2025-10-11 20:33:50 -0700

    Use better mixin methods.

--- src/main/java/com/yiranmushroom/mixin/EntityRendererMixin.java
+++ src/main/java/com/yiranmushroom/mixin/EntityRendererMixin.java
@@ -1,26 +1,20 @@
 package com.yiranmushroom.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.yiranmushroom.mixin_helper.NightVision;
 import net.minecraft.EntityRenderer;
-import net.minecraft.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
-    @Redirect(method = "updateLightmap", at = @At(value = "FIELD", target = "Lnet/minecraft/Minecraft;night_vision_override:Z"))
-    private boolean red$night_vision_override() {
-        return NightVision.getEnabled() || Minecraft.night_vision_override;
+    @ModifyExpressionValue(method = "updateLightmap", at = @At(value = "INVOKE", target = "Lnet/minecraft/EntityClientPlayerMP;isPotionActive(Lnet/minecraft/Potion;)Z"))
+    private boolean mod$isPotionActive(boolean original) {
+        return original || NightVision.getEnabled();
     }
 
-    @Inject(method = "getNightVisionBrightness", at = @At("HEAD"), cancellable = true)
-    private void inj$getNightVisionBrightness(CallbackInfoReturnable<Float> cir) {
-        if (NightVision.getEnabled()) {
-            cir.setReturnValue(1.0f);
-        }
+    @ModifyExpressionValue(method = "updateLightmap", at = @At(value = "INVOKE", target = "Lnet/minecraft/EntityRenderer;getNightVisionBrightness(Lnet/minecraft/EntityPlayer;F)F"))
+    private float mod$getNightVisionBrightness(float original) {
+        return NightVision.getEnabled() ? 1.0f : original;
     }
 }
